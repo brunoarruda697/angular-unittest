6f16f4614eea525742297bd9307062708989b6fa
Bruno Costa <bruno.costa@crmall.com>
2022-04-05 23:58:41 -0300

    teste unitario das funcoes, usando spy e beforeEach pra montar as variaveis antes de cada teste

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,53 +1,28 @@
-import {CalculatorService} from './calculator.service';
-import {LoggerService} from './logger.service';
-import {TestBed} from '@angular/core/testing';
-
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
 
-    let calculator: CalculatorService,
-        loggerSpy: any;
+  let calculatorService: CalculatorService,
+      loggerSpy: any;
 
-    beforeEach(()=> {
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+    calculatorService = new CalculatorService(loggerSpy);
+  })
 
-        console.log("Calling beforeEach");
+  it('should add two numbers', () => {
+    const result = calculatorService.add(2, 2);
 
-        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+    expect(result).toBe(4);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
 
-        TestBed.configureTestingModule({
-            providers: [
-                CalculatorService,
-                {provide: LoggerService, useValue: loggerSpy}
-            ]
-        });
+  it('should subtract two numbers', () => {
+    const result = calculatorService.subtract(2, 2);
 
-        calculator = TestBed.get(CalculatorService);
-
-    });
-
-    it('should add two numbers', () => {
-
-        console.log("add test");
-
-        const result = calculator.add(2, 2);
-
-        expect(result).toBe(4);
-
-        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-
-    });
-
-
-    it('should subtract two numbers', () => {
-
-        console.log("subtract test");
-
-        const result = calculator.subtract(2, 2);
-
-        expect(result).toBe(0, "unexpected subtraction result");
-
-        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-
-    });
+    expect(result).toBe(0);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
 
 });
