0aa6e162ed85c010925c5abc1df0b02944033180
Bruno Costa <bruno.costa@crmall.com>
2022-04-06 17:53:21 -0300

    Testando queryParams

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from "@angular/core/testing";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing"
 import { CoursesService } from "./courses.service"
-import { COURSES } from "../../../../server/db-data";
+import { COURSES, findLessonsForCourse, LESSONS } from "../../../../server/db-data";
 import { Course } from "../model/course";
 import { HttpErrorResponse } from "@angular/common/http";
 
@@ -93,6 +93,25 @@ describe('CoursesService', () => {
     req.flush('Save course failed', {
       status: 500,
       statusText: "Internal server error"
+    })
+  });
+
+  it('should find a list of lessons', () => {
+    coursesService.findLessons(12).subscribe(lessons => {
+      expect(lessons).toBeTruthy();
+      expect(lessons.length).toBe(3);
+    });
+
+    const req = httpTestingController.expectOne(req => req.url == '/api/lessons');
+    expect(req.request.method).toEqual("GET");
+    expect(req.request.params.get("courseId")).toEqual("12");
+    expect(req.request.params.get("filter")).toEqual("");
+    expect(req.request.params.get("sortOrder")).toEqual("asc");
+    expect(req.request.params.get("pageNumber")).toEqual("0");
+    expect(req.request.params.get("pageSize")).toEqual("3");
+
+    req.flush({
+      payload: findLessonsForCourse(12).slice(0,3)
     })
   })
 
